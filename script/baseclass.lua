372f444d5e98770aab0e68a8dc04120eec9c3141
PackMan <pacmanproductionsz25@gmail.com>
2026-08-11 11:12:39 -0600

    update baseclass

--- script/baseclass.lua
+++ script/baseclass.lua
@@ -1,8 +1,14 @@
 local baseWeap = {}
-baseWeap.__index = baseWeap
+
+function baseWeap:toolInfo()
+	self.toolID = "pwb2weap"
+	self.toolName = "PWB2 Baseweapon"
+end
 
 -- Values that ALL weapons share/use
 function baseWeap:initVars()
+	toolInfo()
+
     -- compare against GetTime()
     self.nextFire           = 0
     self.nextAltFire        = 0
@@ -25,24 +31,30 @@ function baseWeap:initVars()
     self.lastFireTime       = 0
 
     -- ammo loaded into weapon. Set by child
+	self.ammo				= 0
     self.ammoLoaded         = 0
     self.ammoAltLoaded      = 0
     
     self.WpnAnimator        = ToolAnimator()
+
+	-- which player owns this
+	self.owner = owner
 end
 
-function baseWeap:new(obj)
+-- literally no clue what exactly this does or how to use it
+function baseWeap:new(obj, owner)
     obj = obj or {}
     setmetatable(obj, self)
+	self.__index = self
 
     -- Set up variables
-    baseWeap:initVars()
+    baseWeap:initVars(owner)
 
     return obj
 end
 
 function baseWeap:CanAttack(attack_time, curtime)
-	return attack_time <= curtime and true or false
+	return (attack_time <= curtime)
 end
 
 WEAPON_NOCLIP = -1
@@ -53,72 +65,67 @@ function baseWeap:SecondaryAttack() end
 function baseWeap:Reload()          end
 
 function baseWeap:PlayEmptySound()
-	--PlaySound("MOD/snd/357_cock1.ogg", POS, 0.8)
+	PlaySound("MOD/snd/357_cock1.ogg", GetPlayerTransform(self.owner).pos, 0.5)
 end
 
 -- TO-DO: add GetToolInfo function that populates a table in the class which sets max clip, name, id and so forth
 --=========================================================================
--- ItemPostFrame - Handles player inputs
+-- tickPlayer - Handles player inputs
 --=========================================================================
-function baseWeap:ItemPostFrame()
+function baseWeap:tickPlayer()
 	local maxClip = self:iMaxClip()
+
+	self.ammo = GetToolAmmo(self.toolID, self.owner)
 
     local curTime = GetTime()
 
 	if self.inReload and m_pPlayer->m_flNextAttack <= curTime then
 		-- complete the reload.
-
 		self.ammoLoaded = math.min(maxClip, m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType])
-
-		--m_pPlayer->TabulateAmmo()
 
 		self.inReload = false
     end
 
     -- declare var for this since it's used a lot 
-    local fireKeyDown = InputDown("usetool", p)
+    local fireKeyDown = InputDown("usetool", self.owner)
 	if not fireKeyDown then
 		m_flLastFireTime = 0.0
     end
 
-	if InputPressed("grab", p) and self:CanAttack(self.nextAltFire, curTime) then
+	if InputPressed("grab", self.owner) and self:CanAttack(self.nextAltFire, curTime) then
 		if self.ammoAltLoaded == 0 then
 			self.fireOnEmpty = true
         end
 
-		--m_pPlayer->TabulateAmmo()
 		self:SecondaryAttack()
 	elseif fireKeyDown and self:CanAttack(self.nextFire, curTime) then
 		if (self.ammoLoaded == 0 and self:pszAmmo1()) or (maxClip == WEAPON_NOCLIP and 0 == GetToolAmmo(TOOLID, p)) then
 			self.fireOnEmpty = true
         end
 
-		--m_pPlayer->TabulateAmmo()
-
 		self:PrimaryAttack()
-	elseif InputPressed("r", p) and maxClip ~= WEAPON_NOCLIP and not self.inReload then
+	elseif InputPressed("r", self.owner) and maxClip ~= WEAPON_NOCLIP and not self.inReload then
 		-- reload when reload is pressed, or if no buttons are down and weapon is empty.
 		self:Reload()
-	elseif not fireKeyDown and not InputDown("grab", p) then
+	elseif not fireKeyDown and not InputDown("grab", self.owner) then
 		-- no fire buttons down
-
 		self.fireOnEmpty = false
 
 		if self:IsUseable() or self.nextFire >= curTime then
 			-- weapon is useable. Reload if empty and weapon has waited as long as it has to after firing
-			if self.ammoLoaded == 0 and self.nextFire < curTime and (iFlags() & ITEM_FLAG_NOAUTORELOAD) == 0 then
+			if self.ammoLoaded == 0 and self.nextFire < curTime and not hasFlag(iFlags(), ITEM_FLAG_NOAUTORELOAD) then
 				self:Reload()
 				return
 			end
 		end
 
-		WeaponIdle()
+		self:WeaponIdle()
 		return
 	end
 
 	-- catch all
-	if ShouldWeaponIdle() then
-		WeaponIdle()
+	if self:ShouldWeaponIdle() then
+		self:WeaponIdle()
 	end
 end
 
