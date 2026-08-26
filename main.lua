4a8cea0f688ef29c280214c4f5a6e80b6719f17a
Pacman09 <pacmanproductionsz25@gmail.com>
2026-08-25 23:49:04 -0600

    update to-do

--- main.lua
+++ main.lua
@@ -111,6 +111,10 @@ end
    -  Source viewpunch is 'laggy'
 
    -  make 1 global animator for all weapons (better for memory)
+
+   -  fix telepathic shotgun pumping 
+
+   - fix grease gun ADS disabling server firing for a little
 ============================================================================================]]
 
 #version 2
